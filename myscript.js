a275b45b81f083fef030c7b1e24e47bb0b9713b7
DarioSplendido <72265133+DarioSplendido@users.noreply.github.com>
2020-12-07 16:35:28 +0000

    Photos + Javascript in Survey page + Contact page
    Added photos in every page + Completed Survey page with javascript action + added details to Contact page

--- myscript.js
+++ myscript.js
@@ -1,1 +1,17 @@
 // js code for Form Page
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = "Every Month";
+
+slider.oninput = function() {
+
+  if (slider.value < 50) {
+    output.innerHTML = "I don't buy posters/prints";
+  } else if (slider.value == 50) {
+    output.innerHTML = "Every Month";
+  } else {
+    output.innerHTML = "Every Week";
+  }
+}
+
